ed8bc2f81c662a11ebfdea46704b207700db2857
asude-demirci <asudemrci@gmail.com>
2025-04-06 00:06:45 +0300

    feat(person-wall): implement Person and Wall classes with basic OOP structure

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,28 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Person person = new Person("John", "Doe", 20);
+        System.out.println("Firstname: " + person.getFirstName());
+        System.out.println("LastName: " + person.getLastName());
+        System.out.println("Age: " + person.getAge());
+        System.out.println("Is Teen: " + person.isTeen());
+        System.out.println();
+
+        Person person1 = new Person("Jane", "Smith", 25, "jane@email.com", "female", "Canadian");
+        System.out.println("Firstname: " + person1.getFirstName());
+        System.out.println("LastName: " + person1.getLastName());
+        System.out.println("Age: " + person1.getAge());
+        System.out.println("Is Teen: " + person1.isTeen());
+        System.out.println("Email: " + person1.email);
+        System.out.println("Gender: " + person1.gender);
+        System.out.println("Nationality: " + person1.nationality);
+
+        Wall wall = new Wall(5, 4);
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+        System.out.println("width= " + wall.getWidth());
+        System.out.println("height= " + wall.getHeight());
+        System.out.println("area= " + wall.getArea());
     }
 }
